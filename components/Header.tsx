959fce2f47342e19f08addc98b514e7c918b6a81
dev <almas1610@gmail.com>
2025-07-28 07:22:24 +0500

    [feature-up]: updated Header button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
+"use client"
+
 import Link from 'next/link'
 import ActiveLink from './ActiveLink'
+import Image from "next/image";
 
 export default function Header() {
+
+    const openMessenger = () => {
+        const phoneNumber = "77017673731";
+        const encodedMessage = encodeURIComponent("Здравствуйте! Интересует ваша услуга");
+
+        window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, "_blank");
+    }
 
     return (
         <div className="mt-6 flex justify-center font-black">
@@ -22,8 +32,11 @@ export default function Header() {
                         </ActiveLink>
                     </nav>
 
-                    <button className="mr-10 self-background-button text-white px-4 py-1.5 rounded-md transition-colors duration-200 cursor-pointer">
-                        Оставить заявку
+                    <button
+                        onClick={openMessenger}
+                        className="flex gap-2 mr-10 bg-[#60D66A] hover:bg-[#29B63F] border-2 border-[#fff] text-white px-4 py-1.5 rounded-md transition-colors duration-200 cursor-pointer">
+                        <Image width={24} height={24} src="/icons/whatsapp.png" alt="whatsapp-icon" />
+                        Связаться
                     </button>
                 </div>
             </header>
