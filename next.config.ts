efb01386d8cb655b18f9ef950ef5f2b2244be4be
dev <almas1610@gmail.com>
2025-07-30 21:31:32 +0500

    [fix-deploy]: unoptimized options & build limitation

--- next.config.ts
+++ next.config.ts
@@ -6,30 +6,9 @@ const nextConfig: NextConfig = {
   compress: true,
 
   images: {
-    formats: ["image/webp", "image/avif"],
+    unoptimized: true,
   },
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
