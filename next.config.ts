36765984d53c474bf07ae039a35e9047967f15f1
dev <almas1610@gmail.com>
2025-07-31 16:28:05 +0500

    [config]: optimize next.config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,37 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
 
+  compress: true,
+
   images: {
-    unoptimized: true,
-  }
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+
+    async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+
+  trailingSlash: false,
 };
 
 export default nextConfig;
